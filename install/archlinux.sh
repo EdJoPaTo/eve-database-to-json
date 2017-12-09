ab878f3ecf9c109a563aec112d3b791655d06075
Edgar Toll <edjopato@gmail.com>
2017-12-09 23:31:37 +0100

    feat: only start mysql, not enable it
    it should be started for now, not be enabled on every boot

--- install/archlinux.sh
+++ install/archlinux.sh
@@ -2,7 +2,7 @@
 sudo pacman --needed --noconfirm -Sy mariadb php
 
 sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
-sudo systemctl enable mysqld.service
+sudo systemctl start mysqld.service
 
 echo
 echo
