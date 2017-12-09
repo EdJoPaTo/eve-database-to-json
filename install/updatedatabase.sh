d768a764fb83bc4d5c2145bf7ed2f3299443a3ed
Edgar Toll <edjopato@gmail.com>
2017-12-09 23:38:46 +0100

    feat: add console echos what is happening

--- install/updatedatabase.sh
+++ install/updatedatabase.sh
@@ -3,6 +3,8 @@ mkdir -p /tmp/evedump
 cd /tmp/evedump
 
 wget -O mysql-latest.tar.bz2 https://www.fuzzwork.co.uk/dump/mysql-latest.tar.bz2
+echo extract…
 tar xjf mysql-latest.tar.bz2
 
+echo inject into database…
 mysql --user=user --password=password evedump < **/*.sql
