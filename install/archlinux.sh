d5637aed8f9956a752ad30527ef08dfef8922e0a
Edgar Toll <edjopato@gmail.com>
2017-12-09 23:30:41 +0100

    feat: initialize the db
    see https://wiki.archlinux.org/index.php/MySQL#Installation

--- install/archlinux.sh
+++ install/archlinux.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo pacman --needed --noconfirm -Sy mariadb php
 
+sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl enable mysqld.service
 
 echo
 echo
 echo !! uncomment extension=mysqli.so at the end of /etc/php/php.ini !!
-
